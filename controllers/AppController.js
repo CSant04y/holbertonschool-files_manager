6b22aae6f9ae06a2e63a4f63a51ffa2fc1a6c9a7
Corbin <forstupidityonly@gmail.com>
2022-01-17 11:16:18 -0600

    ESLint fixed lacking return in static method

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,20 +4,19 @@ import redisClient from '../utils/redis';
 class AppController {
   static getStatus(req, res) {
     // console.log(`This is req: ${req}\n\t\nThis is res: ${res.status}`);
-    if (redisClient.isAlive() && dbClient.isAlive()) {
-      console.log('Condition was met');
-      res.status(200);
-      return res.send({ redis: true, db: true });
-    }
+    if (!redisClient.isAlive() && !dbClient.isAlive()) return null;
+    // console.log('Condition was met');
+    res.status(200);
+    return res.send({ redis: true, db: true });
   }
 
   static async getStats(req, res) {
     const numUsers = await dbClient.nbUsers();
-    //console.log(numUsers);
+    // console.log(numUsers);
     const numFiles = await dbClient.nbFiles();
-    //console.log(numFiles);
-    //res.status(200);
-    res.status(200).send({ users: numUsers, files: numFiles });
+    // console.log(numFiles);
+    // res.status(200);
+    return res.status(200).send({ users: numUsers, files: numFiles });
   }
 }
 
