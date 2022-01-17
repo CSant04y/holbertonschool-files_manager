302b38f3ce38575541a1c05a69625960a0d49f65
Corbin <forstupidityonly@gmail.com>
2022-01-17 10:13:09 -0600

    is alive using this.cli.connected and removed this.cli.connect on line 13

--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,36 @@
 import Redis from 'redis';
+
 const { promisify } = require('util');
 
 class RedisClient {
-
   constructor() {
     this.cli = Redis.createClient();
     this.getAsync = promisify(this.cli.get).bind(this.cli);
     this.setAsync = promisify(this.cli.set).bind(this.cli);
     this.cli.on('error', (err) => {
-        console.log(`Redis client not connected to the server: ${err}`);
-    })
-    this.cli.connect;
-  };
+      console.log(`Redis client not connected to the server: ${err}`);
+    });
+    // this.cli.connect;
+  }
 
   isAlive() {
-    return 'not_working'
+    return this.cli.connected;
   }
 
   async get(key) {
-    let value = await this.getAsync(key);
+    const value = await this.getAsync(key);
     return value;
   }
 
   async set(key, value, duration) {
     await this.setAsync(key, value, 'EX', duration);
-  };
- 
+  }
+
   async del(key) {
     const delAsync = await promisify(this.cli.del).bind(this.cli);
     await delAsync(key);
   }
-};
+}
 
 const redisClient = new RedisClient();
 module.exports = redisClient;
