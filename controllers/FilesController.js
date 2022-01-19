c3633b94605d2862e90c26458054b007356957a6
CSant04y <esquivelcarlos27893@gmail.com>
2022-01-19 14:36:29 -0600

    added new task for 5

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,27 +1,90 @@
-const { ObjectId } = require('mongodb');
-import UsersController from '../controllers/UsersController';
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
+import { v4 as uuidv4 } from 'uuid';
 import getUser from '../utils/getUser';
+
+const { ObjectId } = require('mongodb');
+const fs = require('fs');
+const dbClient = require('../utils/db');
 
 class FilesController {
   static async postUpload(req, res) {
     const userInfo = await getUser(req, res);
-    const { name, type, parentId, isPublic, data } = req.body;
-    if (!name) return res.status(400).json({error: 'Missing name' });
+    console.log('This is userInfo: ', userInfo);
+    const { name, type, data } = req.body;
+    let { isPublic } = req.body;
+    if (!name) return res.status(400).json({ error: 'Missing name' });
     if (!type || ['folder', 'file', 'image'].indexOf(type) === -1) return res.status(400).json({ error: 'Missing type' });
-    if (!data && type != 'folder') return res.status(400).json({error: 'Missing data' });
-    if (!parentId) parentId = 0;
-    else {
-      
+    if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
+    let parentId = req.body.parentId || 0;
+    parentId = parentId === '0' ? 0 : parentId;
+    console.log('This is parent: ', parentId);
+    if (parentId !== 0) {
+      const value = await dbClient.files.findOne({ _id: `ObjectId(${parentId})` });
+      console.log('This os value: ', value);
+      if (!value) return res.status(400).json({ error: 'Parent not found' });
+      console.log('This is value: ', value);
+
+      if (type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
     }
-    
-    console.log('thisis type test after: ',type)
-    console.log('This is name: ',name);
-    console.log('This is type: ',type);
-    console.log('This is data: ',data);
-    console.log('This is isPublic: ',isPublic);
-    console.log('This is parentId: ',parentId);
+    if (type === 'folder') {
+      if (isPublic === undefined) isPublic = false;
+
+      const doc = {
+        userId: userInfo._id,
+        name,
+        type,
+        isPublic,
+        parentId,
+      };
+      const file = await dbClient.files.insertOne(doc);
+
+      const { ops } = file;
+
+      const obj = ops[0];
+
+      return res.status(201).json({
+        id: obj._id,
+        userId: obj.userId,
+        name: obj.name,
+        type: obj.type,
+        isPublic: obj.isPublic,
+        parentId: obj.parentId,
+      });
+    }
+
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+
+    if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath, { resursive: true });
+
+    const fileId = uuidv4();
+
+    const localPath = `${folderPath}/${fileId}`;
+
+    const decodedData = Buffer.from(data, 'base64');
+
+    await fs.promises.writeFile(localPath, decodedData.toString(), { flag: 'w+' });
+
+    const obj = await dbClient.files.insertOne({
+      userId: ObjectId(userInfo._id),
+      name,
+      type,
+      isPublic,
+      parentId,
+      localPath,
+    });
+
+    const { ops } = obj;
+    const obj2 = ops[0];
+
+    return res.status(201).json({
+      id: obj2._id,
+      userId: obj2._id,
+      name: obj2.name,
+      type: obj2.type,
+      isPublic: obj2.isPublic,
+      parentId: obj2.parentId,
+    });
   }
 }
 export default FilesController;
--- utils/getUser.js
+++ utils/getUser.js
@@ -1,19 +1,18 @@
-
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
 const { ObjectId } = require('mongodb');
+const dbClient = require('./db');
+const redisClient = require('./redis');
 
 async function getUser(req, res) {
-    const token = req.header('X-token');
+  const token = req.header('X-token');
 
-    if (!token) return res.status(401).send({ error: 'Unauthorized' });
-    const user = await redisClient.get(`auth_${token}`);
+  if (!token) return res.status(401).send({ error: 'Unauthorized' });
+  const user = await redisClient.get(`auth_${token}`);
 
-    const value = await dbClient.users.findOne({ _id: ObjectId(user) });
+  const value = await dbClient.users.findOne({ _id: ObjectId(user) });
 
-    if (!value) return res.status(401).send({ error: 'Unauthorized' });
+  if (!value) return res.status(401).send({ error: 'Unauthorized' });
 
-    return (value);
+  return (value);
 }
 
 export default getUser;
